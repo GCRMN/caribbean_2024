1ffb5b2c314c17eec58dd7017a62262b1298708a
JWicquart <jeremywicquart@gmail.com>
2024-07-30 22:08:28 +0200

    remove vector bathymetry

--- code/a03_clean_shp.R
+++ code/a03_clean_shp.R
@@ -5,44 +5,9 @@ library(sf)
 sf_use_s2(FALSE) # Switch from S2 to GEOS
 library(nngeo)
 
-# 2. Bathymetry ----
+# 2. Coral reefs ----
 
-## 2.1 List path of shapefiles ----
-
-list_shp <- list.files(path = "data/01_maps/01_raw/04_natural-earth/ne_10m_bathymetry_all/",
-                       pattern = ".shp", full.names = TRUE)
-
-## 2.2 Combine shapefiles ----
-
-data_bathy <- map_dfr(list_shp, ~st_read(., quiet = TRUE)) %>% 
-  mutate(fill_color = case_when(depth == 0 ~ "#e1f5fe",
-                                depth == 200 ~ "#b3e5fc",
-                                depth == 1000 ~ "#81d4fa",
-                                depth == 2000 ~ "#4fc3f7",
-                                depth == 3000 ~ "#29b6f6",
-                                depth == 4000 ~ "#03a9f4",
-                                depth == 5000 ~ "#039be5",
-                                depth == 6000 ~ "#0288d1",
-                                depth == 7000 ~ "#0288d1",
-                                depth == 8000 ~ "#0277bd",
-                                depth == 9000 ~ "#01579b",
-                                depth == 10000 ~ "black")) %>% 
-  mutate(color = fct_reorder(fill_color, depth)) %>% 
-  st_transform(crs = 4326) %>% 
-  st_wrap_dateline() %>% 
-  st_make_valid() %>% 
-  st_transform(crs = 4326) %>% 
-  select(depth, fill_color)
-
-## 2.3 Export the data ----
-
-save(data_bathy, file = "data/01_maps/02_clean/04_natural-earth/ne_10m_bathymetry_all.RData") # RData
-
-rm(list_shp, data_bathy)
-
-# 3. Coral reefs ----
-
-## 3.1 Load data ----
+## 2.1 Load data ----
 
 data_regions <- read_sf("data/01_maps/02_clean/01_gcrmn-regions/gcrmn_regions.shp") %>% 
   st_transform(crs = 4326) %>% 
@@ -56,26 +21,26 @@ data_eez <- read_sf("data/01_maps/01_raw/02_eez/eez_v12.shp") %>%
   st_transform(crs = 4326) %>% 
   st_make_valid()
 
-## 3.2 Filter reefs falling within Caribbean GCRMN region ----
+## 2.2 Filter reefs falling within Caribbean GCRMN region ----
 
 data_reefs <- st_intersection(data_reefs, data_regions) %>%
   filter(region == "Caribbean")
 
-## 3.3 Visual check ----
+## 2.3 Visual check ----
 
 ggplot() +
   geom_sf(data = data_reefs, fill = "red")
 
-# 4. EEZ ----
+# 3. EEZ ----
 
-## 4.1 Filter EEZ with coral reefs ----
+## 3.1 Filter EEZ with coral reefs ----
 
 data_eez_intersects <- st_intersection(data_eez, data_reefs)
 
 data_eez <- data_eez %>% 
   filter(TERRITORY1 %in% unique(data_eez_intersects$TERRITORY1))
 
-## 4.2 Overclaimed territories ----
+## 3.2 Overclaimed territories ----
 
 data_eez2 <- data_eez %>% 
   mutate(overclaimed = ifelse(str_detect(GEONAME, "Over"), TRUE, FALSE))
@@ -90,9 +55,9 @@ ggsave("figs/06_additional/overclaimed-territories.png")
 
 rm(data_eez2)
 
-## 4.3 Remove non-caribbean EEZ ----
+## 3.3 Remove non-caribbean EEZ ----
 
-### 4.3.1 United States ----
+### 3.3.1 United States ----
 
 data_eez <- data_eez %>% 
   filter(SOVEREIGN1 == "United States" & GEONAME == "United States Exclusive Economic Zone") %>%
@@ -103,7 +68,7 @@ data_eez <- data_eez %>%
                          GEONAME == "United States Exclusive Economic Zone")), .) %>% 
   filter(GEONAME != "Joint regime area: United States / Russia")
 
-### 4.3.2 Mexico ----
+### 3.3.2 Mexico ----
 
 data_eez <- data_eez %>% 
   filter(SOVEREIGN1 == "Mexico") %>% 
@@ -112,7 +77,7 @@ data_eez <- data_eez %>%
   bind_rows(data_eez %>% 
               filter(!(SOVEREIGN1 == "Mexico")), .)
   
-### 4.3.3 Colombia ----
+### 3.3.3 Colombia ----
 
 data_eez <- data_eez %>% 
   filter(SOVEREIGN1 == "Colombia" & GEONAME == "Colombian Exclusive Economic Zone") %>% 
@@ -122,7 +87,7 @@ data_eez <- data_eez %>%
               filter(!(SOVEREIGN1 == "Colombia" & 
                          GEONAME == "Colombian Exclusive Economic Zone")), .)
 
-### 4.3.4 Nicaragua ----
+### 3.3.4 Nicaragua ----
 
 data_eez <- data_eez %>% 
   filter(SOVEREIGN1 == "Nicaragua") %>% 
@@ -131,7 +96,7 @@ data_eez <- data_eez %>%
   bind_rows(data_eez %>% 
               filter(!(TERRITORY1 == "Nicaragua")), .)
 
-### 4.3.5 Costa Rica ----
+### 3.3.5 Costa Rica ----
 
 data_eez <- data_eez %>% 
   filter(SOVEREIGN1 == "Costa Rica" & GEONAME == "Costa Rican Exclusive Economic Zone") %>% 
@@ -142,7 +107,7 @@ data_eez <- data_eez %>%
                          GEONAME == "Costa Rican Exclusive Economic Zone")), .) %>% 
   filter(GEONAME != "Joint regime area: Costa Rica / Ecuador (Galapagos)")
 
-### 4.3.6 Panama ----
+### 3.3.6 Panama ----
 
 data_eez <- data_eez %>% 
   filter(SOVEREIGN1 == "Panama") %>% 
@@ -152,7 +117,7 @@ data_eez <- data_eez %>%
   bind_rows(data_eez %>% 
               filter(!(TERRITORY1 == "Panama")), .)
 
-### 4.3.7 Honduras ----
+### 3.3.7 Honduras ----
 
 data_eez <- data_eez %>% 
   filter(GEONAME == "Honduran Exclusive Economic Zone") %>% 
@@ -161,14 +126,14 @@ data_eez <- data_eez %>%
   bind_rows(data_eez %>% 
               filter(!(GEONAME == "Honduran Exclusive Economic Zone")), .)
 
-### 4.3.8 Visual check ----
+### 3.3.8 Visual check ----
 
 ggplot() +
   geom_sf(data = data_eez) +
   geom_sf(data = data_reefs, col = "red") +
   coord_sf(xlim = c(-100, -55), ylim = c(7.5, 35))
 
-### 4.3.9 Remove useless columns ----
+### 3.3.9 Remove useless columns ----
 
 data_eez <- data_eez %>% 
   rename(country = SOVEREIGN1, territory = TERRITORY1) %>% 
@@ -177,7 +142,7 @@ data_eez <- data_eez %>%
   st_transform(crs = 4326) %>% 
   st_make_valid()
 
-### 4.3.10 Remove holes in EEZ ----
+### 3.3.10 Remove holes in EEZ ----
 
 ggplot() +
   geom_sf(data = data_eez)
@@ -199,11 +164,11 @@ data_eez <- data_eez %>%
 ggplot() +
   geom_sf(data = data_eez)
 
-### 4.3.11 Export the data ----
+### 3.3.11 Export the data ----
 
 st_write(data_eez, "data/01_maps/02_clean/03_eez/caribbean_eez.shp", append = TRUE)
 
-## 4.4 Associate EEZ to reefs ----
+## 3.4 Associate EEZ to reefs ----
 
 data_reefs <- data_reefs %>% 
   select(-GRIDCODE) %>% 
@@ -217,7 +182,7 @@ data_reefs <- data_reefs %>%
 
 st_write(data_reefs, "data/01_maps/02_clean/02_reefs/reefs.shp", append = TRUE)
 
-# 5. Land (Princeton) ----
+# 4. Land (Princeton) ----
 
 list_shp <- list.files(path = "data/01_maps/01_raw/05_princeton",
                        pattern = ".shp$", full.names = TRUE, recursive = TRUE)
