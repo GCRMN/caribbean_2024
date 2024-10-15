959e56a52b5d4e71510c6d1f687c9cd9ca85702b
JWicquart <jeremywicquart@gmail.com>
2024-10-15 22:04:24 +0200

    export list of contributors

--- code/a02_benthic-data_sources.R
+++ code/a02_benthic-data_sources.R
@@ -17,3 +17,10 @@ data_benthic %>%
   group_by(territory) %>% 
   summarise(datasetID = paste0(datasetID, collapse = ", ")) %>% 
   openxlsx::write.xlsx(., file = "figs/05_supp-mat/tbl-1.xlsx")
+
+# 4. List of contributors ----
+
+read_xlsx("C:/Users/jwicquart/Desktop/Recherche/03_projects/2022-02-10_gcrmndb_benthos/gcrmndb_benthos/data/05_data-sources.xlsx") %>% 
+  filter(datasetID %in% unique(data_benthic$datasetID)) %>% 
+  select(datasetID, rightsHolder, last_name, first_name) %>% 
+  openxlsx::write.xlsx(., file = "figs/05_supp-mat/tbl-2.xlsx")
