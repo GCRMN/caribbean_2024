69ccb03f0c4a56ff11e67657de3a02ed22f9a4f5
JWicquart <jeremywicquart@gmail.com>
2024-11-24 22:38:48 +0100

    extract relative coral reef extent

--- code/e11_other-indicators.R
+++ code/e11_other-indicators.R
@@ -5,20 +5,73 @@ library(sf)
 sf_use_s2(FALSE)
 library(treemapify)
 library(patchwork)
+library(lwgeom)
 
 # 2. Source functions ----
 
 source("code/function/graphical_par.R")
 source("code/function/theme_graph.R")
 
-# 3. Table 1 - Geographic information ----
+# 3. Relative coral reef extent of the Caribbean ----
 
-## 3.1 Maritime area ----
+## 3.1 Load and transform data ----
 
-data_eez <- st_read("data/01_shp/02_clean/03_eez/caribbean_eez.shp")
+data_reefs <- st_read("data/01_maps/01_raw/01_reefs/reef_500_poly.shp") %>% 
+  st_transform(crs = 4326) %>% 
+  st_wrap_dateline(options = c("WRAPDATELINE=YES")) %>% 
+  st_make_valid()
 
-data_maritime_area <- data_eez %>% 
+## 3.2 Reef area by Caribbean area ----
+
+data_area <- st_read("data/01_maps/02_clean/03_eez/caribbean_area.shp")
+
+data_reef_area_caribbean <- st_intersection(data_area, data_reefs) %>%  
+  group_by(area) %>% 
+  summarise(reef_area_abs = sum(st_area(geometry))) %>% 
+  ungroup() %>% 
   st_drop_geometry() %>% 
-  select(TERRITORY1, AREA_KM2) %>% 
-  rename(territory = TERRITORY1, maritime_area = AREA_KM2) %>% 
-  arrange(territory)
+  mutate(reef_area_abs = as.numeric(reef_area_abs)*1e-6) %>% 
+  arrange(-reef_area_abs)
+
+plot_b <- ggplot(data = data_reef_area_caribbean, aes(area = reef_area_abs, fill = reef_area_abs, label = area)) +
+  geom_treemap(show.legend = FALSE, color = "white", size = 2) +
+  geom_treemap_text(color = "white", place = "centre", reflow = TRUE, family = font_choose_graph) +
+  scale_fill_gradientn(colours = palette_first[3:5]) +
+  theme_graph()
+
+ggsave(filename = "figs/01_part-1/fig-5b.png", plot = plot_b, height = 5, width = 5, dpi = fig_resolution)
+
+## 3.3 Reef area by GCRMN regions ----
+
+load("data/01_maps/02_clean/01_gcrmn-regions/gcrmn_regions.RData")
+
+data_reef_area_gcrmn <- st_intersection(data_gcrmn_regions, data_reefs) %>%  
+  group_by(region) %>% 
+  summarise(reef_area_abs = sum(st_area(geometry))) %>% 
+  ungroup() %>% 
+  st_drop_geometry() %>% 
+  mutate(reef_area_abs = as.numeric(reef_area_abs)*1e-6,
+         region = str_replace_all(region, "EAS", "East Asian Seas"),
+         color = case_when(region == "Caribbean" ~ palette_first[3],
+                           TRUE ~ palette_first[4]),
+         color_text = case_when(region == "Caribbean" ~ "white",
+                                TRUE ~ "white"))
+
+plot_a <- ggplot(data = data_reef_area_gcrmn, aes(area = reef_area_abs, fill = color, label = region)) +
+  geom_treemap(show.legend = FALSE, color = "white", size = 2, start = "bottomleft") +
+  geom_treemap_text(aes(color = color_text), place = "centre", reflow = TRUE,
+                    family = font_choose_graph, start = "bottomleft") +
+  scale_fill_identity() +
+  scale_color_identity() +
+  theme_graph()
+
+ggsave(filename = "figs/01_part-1/fig-5a.png", plot = plot_a, height = 5, width = 5, dpi = fig_resolution)
+
+## 3.4 Combine the two figures ----
+
+(plot_a + labs(title = "A") + theme(plot.margin = unit(c(0.25, 0.75, 0.25, 0), "cm"),
+                                    plot.title = element_text(size = 18))) +
+  (plot_b + labs(title = "B") + theme(plot.margin = unit(c(0.25, 0, 0.25, 0.75), "cm"),
+                                      plot.title = element_text(size = 18)))
+
+ggsave(filename = "figs/01_part-1/fig-5.png", height = 5, width = 10, dpi = fig_resolution)
