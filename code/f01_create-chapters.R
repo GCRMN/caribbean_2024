08e86c369fe211f90300f54dc8f40b204d38a148
JWicquart <jeremywicquart@gmail.com>
2024-12-04 07:53:03 +0100

    include chapter number

--- code/f01_create-chapters.R
+++ code/f01_create-chapters.R
@@ -11,10 +11,12 @@ source("code/function/render_qmd.R")
 # 3. Get list of areas ----
 
 data_area <- st_read("data/01_maps/02_clean/03_eez/caribbean_area.shp") %>% 
-  st_drop_geometry()
+  st_drop_geometry() %>% 
+  arrange(area) %>% 
+  mutate(nb = row_number())
 
 # 4. Create docx files for each area ----
 
 map(unique(data_area$area), ~render_qmd(area_i = .x))
 
-render_qmd(area_i = "Martinique")
+render_qmd(area_i = "Grenada")
--- code/function/render_qmd.R
+++ code/function/render_qmd.R
@@ -6,6 +6,8 @@ render_qmd <- function(area_i){
   
   file_name <- paste0("chapter_", territory_name, ".docx")
   
+  nb_chapter <- data_area %>% filter(area == area_i) %>% select(nb) %>% pull()
+  
   if(file.exists(paste0("doc/", file_name)) == FALSE){
     
     render("code/function/create_chapter_doc.qmd", 
