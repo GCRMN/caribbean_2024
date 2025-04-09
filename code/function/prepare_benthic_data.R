bfeade8a13fa06bd8361f95ef096be1e6468e224
JWicquart <jeremywicquart@gmail.com>
2025-04-09 14:36:22 +0200

    major update on benthic data preparation

--- code/function/prepare_benthic_data.R
+++ code/function/prepare_benthic_data.R
@@ -1,25 +1,137 @@
-prepare_benthic_data <- function(data){
+prepare_benthic_data <- function(data, remove_na_algae){
   
-  result <- data %>% 
-    # 1. Sum of benthic cover per sampling unit (site, transect, quadrat) and category
-    mutate(category = case_when(subcategory == "Macroalgae" ~ "Macroalgae",
-                                subcategory == "Turf algae" ~ "Turf algae",
-                                subcategory == "Coralline algae" ~ "Coralline algae",
-                                TRUE ~ category)) %>% 
-    filter(category %in% c("Hard coral", "Macroalgae", "Turf algae", "Coralline algae", "Other fauna")) %>% 
+  # Main categories
+  
+  result_category <- data %>% 
+    # 1. Filter taxonomic level
+    filter(category %in% c("Hard coral", "Algae", "Other fauna")) %>% 
+    # 2. Sum of benthic cover per sampling unit (site, transect, quadrat) and category
     group_by(datasetID, region, subregion, ecoregion, country, territory, area, locality, habitat, parentEventID,
              decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, eventID, category) %>% 
     summarise(measurementValue = sum(measurementValue)) %>% 
     ungroup() %>% 
-    # 2. Summarise data at the transect level (i.e. mean of photo-quadrats)
+    # 3. Sum of benthic cover per sampling unit (site, transect, quadrat) and category
+    group_by(datasetID, region, subregion, ecoregion, country, territory, area, locality, habitat, parentEventID,
+             decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, eventID, category) %>% 
+    summarise(measurementValue = sum(measurementValue)) %>% 
+    ungroup() %>% 
+    # 4. Regenerate 0 values
+    group_by(datasetID) %>% 
+    complete(category,
+             nesting(region, subregion, ecoregion, country, territory, locality,
+                     habitat, parentEventID, decimalLatitude, decimalLongitude, verbatimDepth,
+                     year, month, day, eventDate),
+             fill = list(measurementValue = 0)) %>%
+    ungroup() %>% 
+    # 5. Average of benthic cover per transect (i.e. mean of photo-quadrats)
     # This avoid getting semi-quantitative data (e.g. when there is only 10 points per photo-quadrat)
     group_by(datasetID, region, subregion, ecoregion, country, territory, area, locality, habitat, parentEventID,
              decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, category) %>% 
     summarise(measurementValue = mean(measurementValue)) %>% 
     ungroup() %>% 
-    # 3. Remove values greater than 100 (unlikely but included to avoid any issues later)
+    # 6. Remove values greater than 100 (unlikely but included to avoid any issues later)
+    filter(measurementValue <= 100)
+  
+  # Algae subcategories
+  
+  # Option 1 - Remove datasets for which at least one row is NA for an algae subcategory
+  result_subcategory <- if(remove_na_algae == TRUE){
+    
+    data %>% 
+      # 1. Filter taxonomic level
+      filter(category == "Algae") %>% 
+      # 2. Remove datasets for which at least one row is NA for an algae subcategory
+      group_by(datasetID) %>%
+      filter(any(is.na(subcategory)) == FALSE) %>% 
+      ungroup() %>% 
+      mutate(category = subcategory) %>% 
+      # 3. Sum of benthic cover per sampling unit (site, transect, quadrat) and category
+      group_by(datasetID, region, subregion, ecoregion, country, territory, area, locality, habitat, parentEventID,
+               decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, eventID, category) %>% 
+      summarise(measurementValue = sum(measurementValue)) %>% 
+      ungroup() %>% 
+      # 4. Regenerate 0 values
+      group_by(datasetID) %>% 
+      complete(category,
+               nesting(region, subregion, ecoregion, country, territory, area, locality,
+                       habitat, parentEventID, decimalLatitude, decimalLongitude, verbatimDepth,
+                       year, month, day, eventDate),
+               fill = list(measurementValue = 0)) %>%
+      ungroup() %>% 
+      # 5. Average of benthic cover per transect (i.e. mean of photo-quadrats)
+      # This avoid getting semi-quantitative data (e.g. when there is only 10 points per photo-quadrat)
+      group_by(datasetID, region, subregion, ecoregion, country, territory, area, locality, habitat, parentEventID,
+               decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, category) %>% 
+      summarise(measurementValue = mean(measurementValue)) %>% 
+      ungroup() %>% 
+      # 6. Remove values greater than 100 (unlikely but included to avoid any issues later)
+      filter(measurementValue <= 100)
+    
+    # Option 2 - Don't remove datasets for which at least one row is NA for an algae subcategory
+    }else{ 
+    
+    data %>% 
+      # 1. Filter taxonomic level
+      filter(category == "Algae") %>% 
+      drop_na(subcategory) %>% 
+      mutate(category = subcategory) %>% 
+      # 2. Sum of benthic cover per sampling unit (site, transect, quadrat) and category
+      group_by(datasetID, region, subregion, ecoregion, country, territory, area, locality, habitat, parentEventID,
+               decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, eventID, category) %>% 
+      summarise(measurementValue = sum(measurementValue)) %>% 
+      ungroup() %>% 
+      # 3. Regenerate 0 values
+      group_by(datasetID) %>% 
+      complete(category,
+               nesting(region, subregion, ecoregion, country, territory, area, locality,
+                       habitat, parentEventID, decimalLatitude, decimalLongitude, verbatimDepth,
+                       year, month, day, eventDate),
+               fill = list(measurementValue = 0)) %>%
+      ungroup() %>% 
+      # 4. Average of benthic cover per transect (i.e. mean of photo-quadrats)
+      # This avoid getting semi-quantitative data (e.g. when there is only 10 points per photo-quadrat)
+      group_by(datasetID, region, subregion, ecoregion, country, territory, area, locality, habitat, parentEventID,
+               decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, category) %>% 
+      summarise(measurementValue = mean(measurementValue)) %>% 
+      ungroup() %>% 
+      # 5. Remove values greater than 100 (unlikely but included to avoid any issues later)
+      filter(measurementValue <= 100)
+  
+  }
+  
+  # Hard coral families
+  
+  result_family <- data %>% 
+    # 1. Filter taxonomic level
+    filter(category == "Hard coral") %>%
+    filter(family %in% c("Poritidae", "Acroporidae", "Meandrinidae", "Milleporidae")) %>% 
+    mutate(category = family) %>% 
+    # 2. Sum of benthic cover per sampling unit (site, transect, quadrat) and category
+    group_by(datasetID, region, subregion, ecoregion, country, territory, area, locality, habitat, parentEventID,
+             decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, eventID, category) %>% 
+    summarise(measurementValue = sum(measurementValue)) %>% 
+    ungroup() %>% 
+    # 3. Regenerate 0 values
+    group_by(datasetID) %>% 
+    complete(category,
+             nesting(region, subregion, ecoregion, country, territory, area, locality,
+                     habitat, parentEventID, decimalLatitude, decimalLongitude, verbatimDepth,
+                     year, month, day, eventDate),
+             fill = list(measurementValue = 0)) %>%
+    ungroup() %>% 
+    # 4. Average of benthic cover per transect (i.e. mean of photo-quadrats)
+    # This avoid getting semi-quantitative data (e.g. when there is only 10 points per photo-quadrat)
+    group_by(datasetID, region, subregion, ecoregion, country, territory, area, locality, habitat, parentEventID,
+             decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, category) %>% 
+    summarise(measurementValue = mean(measurementValue)) %>% 
+    ungroup() %>% 
+    # 5. Remove values greater than 100 (unlikely but included to avoid any issues later)
     filter(measurementValue <= 100)
 
+  # Combine data
+  
+  result <- bind_rows(result_category, result_subcategory, result_family)
+  
   return(result)
   
 }
