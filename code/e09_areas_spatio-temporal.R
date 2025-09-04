a38e9b48de6d74bd97bae70668391068e3696314
JWicquart <jeremywicquart@gmail.com>
2025-09-04 13:33:31 +0200

    add banks for USVI map

--- code/e09_areas_spatio-temporal.R
+++ code/e09_areas_spatio-temporal.R
@@ -197,6 +197,68 @@ plot_map_area <- function(area_i){
       theme_map_area() +
       labs(x = NULL, y = NULL)
     
+  }else if(area_i == "United States Virgin Islands"){
+    
+    # Create polygons for MPA
+    
+    data_mpa <- tibble(point = c("A", "B", "C", "D"),
+                       latitude = c(18.1983, 18.19408, 18.1843, 18.18852),
+                       longitude = c(-64.9388, -64.93708, -64.9635, -64.96522))
+    
+    coords <- data_mpa %>%
+      select(longitude, latitude) %>%
+      as.matrix() %>%
+      rbind(c(data_mpa$longitude[1], data_mpa$latitude[1]))
+    
+    data_mpa_1 <- st_sf(mpa = "Grammanik Bank",
+                        geometry = st_sfc(st_polygon(list(coords)), crs = 4326))
+    
+    data_mpa <- tibble(point = c("A", "B", "C", "D"),
+                       latitude = c(18.22, 18.22, 18.19667, 18.17833),
+                       longitude = c(-65.1, -64.98333, -64.98333, -65.1))
+    
+    coords <- data_mpa %>%
+      select(longitude, latitude) %>%
+      as.matrix() %>%
+      rbind(c(data_mpa$longitude[1], data_mpa$latitude[1]))
+    
+    data_mpa_2 <- st_sf(mpa = "Hind Bank",
+                        geometry = st_sfc(st_polygon(list(coords)), crs = 4326))
+    
+    data_mpa <- bind_rows(data_mpa_1, data_mpa_2)
+    
+    plot_i <- ggplot() +
+      geom_sf(data = data_land, fill = "#dadfe1", color = "darkgrey", linewidth = 0.1) +
+      geom_sf(data = data_benthic %>% filter(area == area_i) %>% arrange(int_class) %>% 
+                # Add point outside map range to show the legend when no data
+                bind_rows(., tibble(int_class = unique(data_benthic$int_class),
+                                    lat = 0,
+                                    long = 0) %>% 
+                            st_as_sf(coords = c("long", "lat"), crs = 4326)),
+              size = 1.5,
+              aes(color = int_class), show.legend = TRUE) +
+      scale_color_manual(values = palette_second,
+                         breaks = c("1 year", "2-5 years", "6-10 years", "11-15 years", ">15 years"),
+                         labels = c("1 year", "2-5 years", "6-10 years", "11-15 years", ">15 years"), 
+                         drop = FALSE,
+                         name = "NUMBER OF YEARS\nWITH DATA") +
+      geom_sf(data = data_mpa, color = "black", fill = "transparent", linetype = "dashed") +
+      guides(color = guide_legend(override.aes = list(size = 3.5))) +
+      annotation_scale(location = as.character(data_params_i$scalebar_pos),
+                       width_hint = 0.25, text_family = font_choose_map, text_col = "#2c3e50",
+                       text_cex = 0.7, style = "bar", line_width = 1, height = unit(0.04, "cm"), line_col = "#2c3e50",
+                       pad_x = unit(0.5, "cm"), pad_y = unit(0.5, "cm"), bar_cols = c("#2c3e50", "#2c3e50")) +
+      coord_sf(xlim = c(as.numeric(data_params_i$xmin), as.numeric(data_params_i$xmax)),
+               ylim = c(as.numeric(data_params_i$ymin), as.numeric(data_params_i$ymax)),
+               expand = FALSE,
+               label_axes = list(bottom = "E", top = "E", left = "N")) +
+      scale_x_continuous(breaks = eval(parse(text = data_params_i$scale_x))) +
+      scale_y_continuous(breaks = eval(parse(text = data_params_i$scale_y))) +
+      theme_map_area() +
+      labs(x = NULL, y = NULL)
+    
+    rm(data_mpa, data_mpa_1, data_mpa_2, coords)
+   
   }else{
     
     plot_i <- ggplot() +
